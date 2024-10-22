71b792e59f7183dee154b1b8168d99ea851ef6d9
primeshuke <ayanoadishukla@gmail.com>
2024-10-22 19:50:28 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='https://t.me/allanimesschannel'>This Channel</a>\n○ Movies Channel : <a href='https://t.me/Moviez_Explorer'>Moviez Explorer</a>\n○ Support Bot : <a href='https://t.me/ExplorerContactBot'>Contact Us</a></b>",
+            text = f"<b>○ Creator : <a href='https://t.me/CinemaStack_Official'>This Channel</a>\n○ Movies Channel : <a href='https://t.me/CinemaStack_Official'>Moviez Explorer</a>\n○ Anime Channel : <a href='https://t.me/AnimePlaza_STR'>Moviez Explorer</a>\n○ Support: <a href='https://t.me/aCavilrine'>Contact Us</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
